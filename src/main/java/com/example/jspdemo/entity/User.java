31b11d0881986a99e37b3968bbc2e72131bc0229
bdqn <shye200@163.com>
2022-06-07 15:41:19 +0800

    User com6

--- src/main/java/com/example/jspdemo/entity/User.java
+++ src/main/java/com/example/jspdemo/entity/User.java
@@ -1,13 +1,14 @@
 package com.example.jspdemo.entity;
 
 public class User {
+
     private Integer id;//1123
 
     public Integer getId() {
         return id;
-    }//get封装
+    }//Get 封装
 
     public void setId(Integer id) {
         this.id = id;
-    }
+    }//set封装
 }
